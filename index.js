b3775726c052c07ee5952785ca6a7f9dae2b1ffe
eniallator <eniallatormc@gmail.com>
2016-08-23 19:22:02 +0100

    Basic chat functionality with connect/chat/disconnect

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require('express');
-
 const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const users = {}
 
 require('dotenv').config();
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id)
+
+  socket.on("login", (username) => {
+    users[socket.id] = username
+    io.emit("chat message", username + " has connected.")
+
+  })
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(socket.id)
+    io.emit('chat message', users[socket.id] + " has disconnected.");
+  })
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+app.get('/jquery.js', (req, res) => {
+  res.sendFile(__dirname + '/node_modules/jquery/dist/jquery.slim.min.js');
+});
 
-app.listen(process.env.PORT, () => {
+http.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}!`);
 });
